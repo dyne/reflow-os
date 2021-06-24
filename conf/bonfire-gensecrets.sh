f6eb05bb2d850d098baa0ebbb606357a9a1c4957
Adam Burns <adamb@free2air.net>
2021-06-24 07:44:19 +0200

    fixes null byte in input warnings

--- conf/bonfire-gensecrets.sh
+++ conf/bonfire-gensecrets.sh
@@ -2,25 +2,25 @@
 
 cat << EOF
 # for sessions/cookies
-SECRET_KEY_BASE="`echo "print(OCTET.random(64):base58())" |  ./zenroom 2>/dev/null`"
-SIGNING_SALT="`echo "print(OCTET.random(64):base58())" |  ./zenroom 2>/dev/null`"
-ENCRYPTION_SALT="`echo "print(OCTET.random(64):base58())" |  ./zenroom 2>/dev/null`"
+SECRET_KEY_BASE=$( (echo "print(OCTET.random(64):base58())" | ./zenroom 2>/dev/null) | tr -d '\0' )
+SIGNING_SALT=$( (echo "print(OCTET.random(64):base58())" | ./zenroom 2>/dev/null) | tr -d '\0' )
+ENCRYPTION_SALT=$( (echo "print(OCTET.random(64):base58())" | ./zenroom 2>/dev/null) | tr -d '\0' )
 
 # database access
 POSTGRES_USER=postgres
 POSTGRES_DB=bonfire_db
-POSTGRES_PASSWORD=`echo "print(OCTET.random(64):base58())" |  ./zenroom 2>/dev/null`
+POSTGRES_PASSWORD=$( (echo "print(OCTET.random(64):base58())" | ./zenroom 2>/dev/null) | tr -d '\0' )
 
 # signup to mailgun.com and edit with your domain and API key
 MAIL_DOMAIN=mailg.mydomain.net
 MAIL_KEY=123
 
 # password for the search index
-MEILI_MASTER_KEY=`echo "print(OCTET.random(64):base58())" |  ./zenroom 2>/dev/null`
+MEILI_MASTER_KEY=$( (echo "print(OCTET.random(64):base58())" | ./zenroom 2>/dev/null) | tr -d '\0' )
 
 # password for the default admin user if you run the seeds
 SEEDS_USER=root
-SEEDS_PW=`echo "print(OCTET.random(64):base58())" |  ./zenroom 2>/dev/null`
+SEEDS_PW=$( (echo "print(OCTET.random(64):base58())" | ./zenroom 2>/dev/null) | tr -d '\0' )
 
 # backend stuff
 ERLANG_COOKIE=bonfire_cookie
